b69a77a16f6a30e1370ea35e7a3a466261a8b295
Mike Shea <mshea424@yahoo.com>
2020-06-29 14:04:16 -0400

    nav tabs imporved

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 document.querySelector(`#about-tab`).addEventListener(`click`, () => { 
     document.querySelector(`#skills`).style.display = `none`;
+    document.querySelector(`#skills-tab`).className = `nav-tab`;
     document.querySelector(`#projects`).style.display = `none`;
+    document.querySelector(`#projects-tab`).className = `nav-tab`;
     document.querySelector(`#contact`).style.display = `none`;
+    document.querySelector(`#contact-tab`).className = `nav-tab`;
+
     document.querySelector(`#about`).style.display = `block`;
- })
+    document.querySelector(`#about-tab`).className = `left-tab-active`
+})
 
 document.querySelector(`#skills-tab`).addEventListener(`click`, () => { 
     document.querySelector(`#about`).style.display = `none`;
+    document.querySelector(`#about-tab`).className = `nav-tab`;
     document.querySelector(`#projects`).style.display = `none`;
+    document.querySelector(`#projects-tab`).className = `nav-tab`;
     document.querySelector(`#contact`).style.display = `none`;
-    document.querySelector(`#skills`).style.display = `flex` })
+    document.querySelector(`#contact-tab`).className = `nav-tab`;
+    
+    document.querySelector(`#skills`).style.display = `flex`;
+    document.querySelector(`#skills-tab`).className = `middle-tab-active`
+})
 
-document.querySelector(`#projects-tab`).addEventListener(`click`, () => { 
+ document.querySelector(`#projects-tab`).addEventListener(`click`, () => { 
     document.querySelector(`#skills`).style.display = `none`;
+    document.querySelector(`#skills-tab`).className = `nav-tab`;
     document.querySelector(`#about`).style.display = `none`;
+    document.querySelector(`#about-tab`).className = `nav-tab`;
     document.querySelector(`#contact`).style.display = `none`;
-    document.querySelector(`#projects`).style.display = `flex` })
+    document.querySelector(`#contact-tab`).className = `nav-tab`;
+    
+    document.querySelector(`#projects`).style.display = `block`;
+    document.querySelector(`#projects-tab`).className = `middle-tab-active`
+})
 
-document.querySelector(`#contact-tab`).addEventListener(`click`, () => { 
+ document.querySelector(`#contact-tab`).addEventListener(`click`, () => { 
     document.querySelector(`#skills`).style.display = `none`;
+    document.querySelector(`#skills-tab`).className = `nav-tab`;
     document.querySelector(`#projects`).style.display = `none`;
+    document.querySelector(`#projects-tab`).className = `nav-tab`;
     document.querySelector(`#about`).style.display = `none`;
-    document.querySelector(`#contact`).style.display = `block` })
+    document.querySelector(`#about-tab`).className = `nav-tab`;
+    
+    document.querySelector(`#contact`).style.display = `block`;
+    document.querySelector(`#contact-tab`).className = `right-tab-active`
+})
